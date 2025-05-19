821a90e1e0d97e1c7c1c76b6c57a9c3b7252be9e
GHTAKAHIRO <takahiro.yanagihara@sozemi.jp>
2025-05-19 21:16:27 +0900

    flask import session

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask import session
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_cors import CORS
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
