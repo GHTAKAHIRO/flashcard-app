b989e493accd0f43e8fa203899be3d9e70695b72
GHTAKAHIRO <takahiro.yanagihara@sozemi.jp>
2025-05-31 14:33:42 +0900

    チャンク機能追加1

--- app.py
+++ app.py
@@ -735,5 +735,5 @@ def reset_history(source):
     return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.
+   port = int(os.environ.get('PORT', 10000))
+   app.run(host='0.0.0.0', port=port)
